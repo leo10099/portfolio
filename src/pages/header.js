8f30c7f4c31f9bd55c100c842c8fa996b2a59178
leo10099 <hancleandro@gmail.com>
2019-01-20 21:57:44 -0300

    first commit

--- src/pages/header.js
+++ src/pages/header.js
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 import ClippedBackground from '../components/styled/clippedBackground'
 import AbsoluteCenter from "../components/styled/mixins/absoluteCenter"
 
+// SVG Images
+
+import github from "../images/github.svg"
+import linkedin from "../images/linkedin.svg"
+import twitter from "../images/twitter.svg"
+
+
 const HeaderContent = styled.div`
   color: ${({ theme }) => theme.light1};
   font-size: 8rem;
@@ -26,6 +33,14 @@ const HeaderContent = styled.div`
     left:2rem;
   }
 
+  .image-container{
+    
+    height:100%;
+    width:33%;
+    display:inline-flex;
+    justify-content:space-around;
+  }
+
   .color-primary {
     color:${({ theme }) => theme.darkBlue}
   }
@@ -37,6 +52,11 @@ export default function Header() {
       <HeaderContent>
         <h1 className="title cursive">Leandro Hanc</h1>
         <h2 className="subtitle"><span className="color-primary">Web Developer</span></h2>
+        <div className="image-container">
+          <img src={github} alt="Github" />
+          <img src={linkedin} alt="Linkeding" />
+          <img src={twitter} alt="Twitter" />
+        </div>
       </HeaderContent>
     </ClippedBackground>
   )
